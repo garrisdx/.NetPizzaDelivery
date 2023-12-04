bf4ef6f7f9db319ef4c20de376ccfab563bad832
Dakota <dakotagarrison.14@gmail.com>
2023-12-04 11:29:32 -0600

    first commit

---
+++ Crust.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Reflection.Metadata;
+
+
+namespace PizzaDelivery
+{
+    public class Crust
+    {
+        [Parameter]
+        public bool Thin { get; set; }
+        [Parameter]
+        public bool Pan { get; set; }
+
+        [Parameter]
+        public bool HandTossed { get; set;}
+
+        public double CrustPrice()
+        {
+            double total = 0;
+            if(Thin)
+            {
+                total += 5.99;
+            }
+            if(Pan)
+            {
+                total += 7.99;
+            }
+            if(HandTossed)
+            {
+                total += 6.99;
+            }
+            return total;
+        }
+        
+    }
+}
---
+++ Pizza.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using static PizzaDelivery.PizzaTop;
+
+namespace PizzaDelivery
+{
+    public class Pizza
+    {
+        [Parameter]
+        public PizzaTop Topping { get; set; }
+
+        [Parameter]
+        public Crust Crust { get; set; }
+        public Pizza()
+        {
+            Topping = new PizzaTop();
+            Crust = new Crust();
+        }
+
+        public double GetFinalPrice()
+        {
+            return Topping.ToppingPrice() + Crust.CrustPrice();
+        }
+    }
+}
